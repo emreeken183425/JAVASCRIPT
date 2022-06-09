b591d2da50004d82fe46a8ac77e53d3e9804eb4f
emreeken183425 <emreeken1834@gmail.com>
2022-06-09 17:47:59 +0300

    loading

---
+++ döngüler.js
@@ -0,0 +1,24 @@
+console.log("****merhaba***  ");
+
+/* while(koşul){
+    çaşışacak kodlar
+}  */
+
+let deger=5;
+
+while( deger<=10 ){
+    document.write(deger+"<br/> ")
+    console.log(deger );
+    deger++;
+  
+}
+
+// *******DO WHİLE******
+
+let sonuc=5;
+
+do{
+    document.write(sonuc)
+    console.log(sonuc );
+    sonuc++;
+}while(sonuc<15);
