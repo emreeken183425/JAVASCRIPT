07f4823397f1328c2ef677107c79c92c35cda89d
emreeken183425 <emreeken1834@gmail.com>
2022-06-10 20:26:58 +0300

    ekleme

--- döngüler.js
+++ döngüler.js
@@ -18,7 +18,72 @@ while( deger<=10 ){
 let sonuc=5;
 
 do{
-    document.write(sonuc)
+    document.write(sonuc+"<br/>")
     console.log(sonuc );
     sonuc++;
 }while(sonuc<15);
+
+// ***FOR****
+/* 
+1.parametre=döngü başlangıç değeri
+2.parametre=koşul
+3.parametre=artırma/azaltma
+for(1.parametre,2.parametre,3.parametre  ){
+    kodlar
+}
+*/
+for(let eken=1; eken<5;eken++ ){
+    document.write(eken)
+}
+
+
+
+// ***FOR İN ****
+
+/* 
+1.parametre=döngü başlangıç değeri
+2.parametre=koşul
+for(1.parametre in 2.parmetre ){
+    kodlar
+}
+*/
+
+
+let liste=["emre","yasemin","emir","eken" ];
+let i;
+
+for(i in liste ){
+    if(liste[i]=="emre" ){
+        console.log("emre benim");
+    }else if(liste[i]=="yasemin" ){
+        console.log("emrenin sevgili eşi yasemin");
+    }else if(liste[i]=="eken" ){
+        console.log("bizim soyadımız");
+    }else if(liste[i]="emir" ){
+        console.log("emir bizim ilk evladımız");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
