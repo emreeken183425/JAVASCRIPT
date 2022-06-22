4865e7ed475fc7c72ac3b56a6dcf47e024a66649
emreeken183425 <emreeken1834@gmail.com>
2022-06-22 14:16:16 +0300

    loading

--- javascript bases/matematiksel.js
+++ javascript bases/matematiksel.js
@@ -12,3 +12,37 @@ console.log(sayı4);
 
 let sayı5=Math.LN2// 2 sayısının logaritmik değerini döndürür.
 console.log(sayı5);
+/*
+E	E sayısını verir. (yaklaşık. 2.718)
+LN2	Logaritma 2yi verir. (yaklaşık. 0.693)
+LN10	Logaritma 10 verir. (yaklaşık. 2.302)
+LOG2E	2 tabanında logritma E verir. (yaklaşık. 1.442)
+LOG10E	On tabanında logaritma E (yaklaşık. 0.434)
+PI	PI sabiti (yaklaşık . 3.14)
+SQRT1_2	Karekök 2yi verir. (yaklaşık. 0.707)
+SQRT2	2nin karesi verir. (yaklaşık. 1.414)*/
+
+//Yukarı doğru en yakın tam sayıya yuvarlama için kullanılır.
+Math.ceil(1.4);  //sonuç 2
+
+//Aşağı doğru en yakın tam sayıya yuvarlar.
+Math.floor(1.6);  // 1
+
+//JavaScript max() Kullanımı
+// Kendisine parametre olarak verilen değerlerden en büyüğünü döndürür.
+Math.max(5, 10,3,25); //25
+
+
+//Kendisine parametre olarak verilen değerlerden en küçüğünü döndürür.
+Math.max(5, 10,3,25); //3
+
+//Sayının kuvvetini hesaplamak için kullanılır. Diğer bir ifade ile üs alma fonksiyonudur.
+Math.pow(4, 3);   //4*4*4
+
+//0-1 arasında rastgele bir sayı üretir.
+ 
+Math.random();
+
+// Sayıyı en yakın tam sayıya yuvarlar.
+Math.round(2.51);  //3
+Math.round(2.49);  //2
