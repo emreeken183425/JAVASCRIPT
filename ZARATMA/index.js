67734571d31dd93f6fecfb66ac4be6c26e23e95e
emreeken183425 <emreeken1834@gmail.com>
2022-08-10 11:49:53 +0300

    fdsf

--- ZARATMA/index.js
+++ ZARATMA/index.js
@@ -2,29 +2,29 @@ function yenizar(){
     let rastgelesayıbir= Math.ceil(Math.random()*6 );
     let rastgelesayıiki= Math.ceil(Math.random()*6 );
     if(rastgelesayıbir==1){
-        document.getElementById('birincizar').src="siyah1.png"
+        document.getElementById('birincizar').src="zar1.png"
     }else if(rastgelesayıbir==2){
-        document.getElementById('birincizar').src="siyah2.png"
+        document.getElementById('birincizar').src="zar2.png"
     }else if(rastgelesayıbir==3){
-        document.getElementById('birincizar').src="siyah3.png"
+        document.getElementById('birincizar').src="zar3.png"
     }else if(rastgelesayıbir==4){
-        document.getElementById('birincizar').src="siyah4.png "
+        document.getElementById('birincizar').src="zar4.png "
     }else if(rastgelesayıbir==5){
-        document.getElementById('birincizar').src="siyah5.png"
+        document.getElementById('birincizar').src="zar5.png"
     }else if(rastgelesayıbir==6){
-        document.getElementById('birincizar').src="siyah6.png"
+        document.getElementById('birincizar').src="zar6.png"
     }
     if(rastgelesayıiki==1){
-        document.getElementById('ikincizar').src="siyah1.png"
+        document.getElementById('ikincizar').src="zar1.png"
     }else if(rastgelesayıiki==2){
-        document.getElementById('ikincizar').src="siyah2.png"
+        document.getElementById('ikincizar').src="zar2.png"
     }else if(rastgelesayıiki==3){
-        document.getElementById('ikincizar').src="siyah3.png"
+        document.getElementById('ikincizar').src="zar3.png"
     }else if(rastgelesayıiki==4){
-        document.getElementById('ikincizar').src="siyah4.png"
+        document.getElementById('ikincizar').src="zar4.png"
     }else if(rastgelesayıiki==5){
-        document.getElementById('ikincizar').src="siyah5.png"
+        document.getElementById('ikincizar').src="zar5.png"
     }else if(rastgelesayıiki==6){
-        document.getElementById('ikincizar').src="siyah6.png"
+        document.getElementById('ikincizar').src="zar6.png"
     }
 }
