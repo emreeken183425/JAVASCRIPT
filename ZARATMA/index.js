0ec8f1b99e5622295a2ef56fe00370350ee9554f
emreeken183425 <emreeken1834@gmail.com>
2022-08-10 11:18:53 +0300

    new

---
+++ ZARATMA/index.js
@@ -0,0 +1,30 @@
+function yenizar(){
+    let rastgelesayıbir= Math.ceil(Math.random()*6 );
+    let rastgelesayıiki= Math.ceil(Math.random()*6 );
+    if(rastgelesayıbir==1){
+        document.getElementById('birincizar').src="siyah1.png"
+    }else if(rastgelesayıbir==2){
+        document.getElementById('birincizar').src="siyah2.png"
+    }else if(rastgelesayıbir==3){
+        document.getElementById('birincizar').src="siyah3.png"
+    }else if(rastgelesayıbir==4){
+        document.getElementById('birincizar').src="siyah4.png "
+    }else if(rastgelesayıbir==5){
+        document.getElementById('birincizar').src="siyah5.png"
+    }else if(rastgelesayıbir==6){
+        document.getElementById('birincizar').src="siyah6.png"
+    }
+    if(rastgelesayıiki==1){
+        document.getElementById('ikincizar').src="siyah1.png"
+    }else if(rastgelesayıiki==2){
+        document.getElementById('ikincizar').src="siyah2.png"
+    }else if(rastgelesayıiki==3){
+        document.getElementById('ikincizar').src="siyah3.png"
+    }else if(rastgelesayıiki==4){
+        document.getElementById('ikincizar').src="siyah4.png"
+    }else if(rastgelesayıiki==5){
+        document.getElementById('ikincizar').src="siyah5.png"
+    }else if(rastgelesayıiki==6){
+        document.getElementById('ikincizar').src="siyah6.png"
+    }
+}
