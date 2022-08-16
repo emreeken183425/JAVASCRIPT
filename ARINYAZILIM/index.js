6c9ea570c3b1bc15f0163757168e070961b44c55
emreeken183425 <emreeken1834@gmail.com>
2022-08-16 17:16:04 +0300

    vxc

--- ARINYAZILIM/index.js
+++ ARINYAZILIM/index.js
@@ -29,3 +29,24 @@ console.log(person);
 console.log(person.name);
 console.log(person.age);
 console.log(person.fullname());
+
+//*  CONSTRUCTURE METHOD
+
+function Person(name,surname,age){
+    const obj={};
+    obj.name=name;
+    obj.surname=surname;
+    obj.age=age;
+
+    return obj;
+}
+const person1=new Person("emre","eken",30)
+console.log(person1);
+const person2=new Person("yasemin","eken",28)
+console.log(person2);
+
+const mywar=new Person("emir","eken",1)
+console.log(mywar);
+
+Person.prototype.friends=["eda","ebru","seda"]
+console.log(mywar);
