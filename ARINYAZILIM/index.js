cfb6a230a68f4f58709f7d8eb6a6e56d759d2eb2
emreeken183425 <emreeken1834@gmail.com>
2022-08-21 11:51:17 +0300

    sınav

--- ARINYAZILIM/index.js
+++ ARINYAZILIM/index.js
@@ -1,52 +1,62 @@
 
 
-// hoisting
+// // hoisting
 
-console.log(x);
-var x=10;
-console.log(x);
+// console.log(x);
+// var x=10;
+// console.log(x);
 
-// function decleration
-function square(num){
-    return (num*num);
-}
-console.log(square(5));
+// // function decleration
+// function square(num){
+//     return (num*num);
+// }
+// console.log(square(5));
 
-const person={
-    name:"jhon",
-    surname:"doe",
-    age:"40",
-    languages:["turkis","english","spanish"],
-    fullname:function(){
-        return this.name +" "+ this.surname
-    },
-    address:{
-        city:"balıkesir",
-        district:"akçay"
-    }
-}
-console.log(person);
-console.log(person.name);
-console.log(person.age);
-console.log(person.fullname());
+// const person={
+//     name:"jhon",
+//     surname:"doe",
+//     age:"40",
+//     languages:["turkis","english","spanish"],
+//     fullname:function(){
+//         return this.name +" "+ this.surname
+//     },
+//     address:{
+//         city:"balıkesir",
+//         district:"akçay"
+//     }
+// }
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.fullname());
 
-//*  CONSTRUCTURE METHOD
+// //*  CONSTRUCTURE METHOD
 
-function Person(name,surname,age){
-    const obj={};
-    obj.name=name;
-    obj.surname=surname;
-    obj.age=age;
+// function Person(name,surname,age){
+//     const obj={};
+//     obj.name=name;
+//     obj.surname=surname;
+//     obj.age=age;
 
-    return obj;
-}
-const person1=new Person("emre","eken",30)
-console.log(person1);
-const person2=new Person("yasemin","eken",28)
-console.log(person2);
+//     return obj;
+// }
+// const person1=new Person("emre","eken",30)
+// console.log(person1);
+// const person2=new Person("yasemin","eken",28)
+// console.log(person2);
 
-const mywar=new Person("emir","eken",1)
-console.log(mywar);
+// const mywar=new Person("emir","eken",1)
+// console.log(mywar);
 
-Person.prototype.friends=["eda","ebru","seda"]
-console.log(mywar);
+// Person.prototype.friends=["eda","ebru","seda"]
+// console.log(mywar);
+
+
+//SNYC AND ASYNC FUNCTİON
+console.log("merhaba");
+
+let x="1"
+let y=2
+let z=3
+console.log(x+y+z);
+
